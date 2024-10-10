271fd2851d6ba91c7a0de32483cd2113d64788f2
HP <g00433839@atu.ie>
2024-10-10 16:09:36 +0100

    final code

--- src/main/java/PersonApp.java
+++ src/main/java/PersonApp.java
@@ -2,20 +2,51 @@ import java.util.Scanner;
 
 public class PersonApp {
     public static void main(String[] args) {
-        System.out.println("Enter in a Name: ");
         Scanner sc = new Scanner(System.in);
+        // promptting for user1 input
+        System.out.println("Enter in a Name: ");
         String name = sc.nextLine();
         System.out.println("Please enter your email: ");
         String email = sc.nextLine();
         System.out.println("Please enter your Course: ");
         String course = sc.nextLine();
 
+        // promptting for user2 input
+        System.out.println("Enter in a Name: ");
+        String name2 = sc.nextLine();
+        System.out.println("Please enter your email: ");
+        String email2 = sc.nextLine();
+        System.out.println("Please enter your Course: ");
+        String course2 = sc.nextLine();
+
+        // promptting for user3 input
+        System.out.println("Enter in a Name: ");
+        String name3 = sc.nextLine();
+        System.out.println("Please enter your email: ");
+        String email3 = sc.nextLine();
+        System.out.println("Please enter your Course: ");
+        String course3 = sc.nextLine();
+
+        //first student
         person n = new person();
         n.setName(name);
         n.setEmail(email);
         n.setCourse(course);
 
+        //second student
+        person n2 = new person();
+        n2.setName(name2);
+        n2.setEmail(email2);
+        n2.setCourse(course2);
+
+
+        //third student
+        person n3 = new person("billybob", "billybob@atu.ie", "SW and Elec Eng");
+        System.out.println(n);
+        System.out.println(n2);
+        System.out.println(n3);
+
 
     }
-    }
+}
 
